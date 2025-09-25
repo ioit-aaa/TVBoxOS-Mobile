006f10d6ea02abfdc9b71a87a51d4c904f13a764
ioit-aaa <211367176+ioit-aaa@users.noreply.github.com>
2025-09-25 11:52:27 +0800

    重写弹窗组件
    Signed-off-by: ioit-aaa <211367176+ioit-aaa@users.noreply.github.com>

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/SearchSuggestionsDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/SearchSuggestionsDialog.java
@@ -8,65 +8,61 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 
 import com.github.tvbox.osc.R;
+import com.google.android.flexbox.FlexboxLayout;
+import com.google.android.flexbox.FlexDirection;
+import com.google.android.flexbox.FlexWrap;
+import com.google.android.flexbox.JustifyContent;
 import com.lxj.xpopup.impl.PartShadowPopupView;
 import com.lxj.xpopup.interfaces.OnSelectListener;
-import com.zhy.view.flowlayout.FlowLayout;
-import com.zhy.view.flowlayout.TagAdapter;
-import com.zhy.view.flowlayout.TagFlowLayout;
 
 import java.util.List;
 
-/**
- * Description: 自定义局部阴影弹窗
- * Create by dance, at 2018/12/21
- */
 public class SearchSuggestionsDialog extends PartShadowPopupView {
 
     private List<String> mList;
     private OnSelectListener onSelectListener;
-    private TagFlowLayout mFl;
+    private FlexboxLayout mFl;
 
     public SearchSuggestionsDialog(@NonNull Context context, List<String> list, OnSelectListener onSelectListener) {
         super(context);
         mList = list;
         this.onSelectListener = onSelectListener;
     }
+
     @Override
     protected int getImplLayoutId() {
         return R.layout.dialog_search_uggestions;
     }
 
-    TextView text;
     @Override
     protected void onCreate() {
         super.onCreate();
         mFl = findViewById(R.id.fl_suggest);
+        mFl.setFlexDirection(FlexDirection.ROW);
+        mFl.setFlexWrap(FlexWrap.WRAP);
+        mFl.setJustifyContent(JustifyContent.FLEX_START);
         updateSuggestions(mList);
-        mFl.setOnTagClickListener(new TagFlowLayout.OnTagClickListener() {
-            @Override
-            public boolean onTagClick(View view, int position, FlowLayout parent) {
-                if (onSelectListener!=null){
-                    onSelectListener.onSelect(position,mList.get(position));
-                }
-                return true;
-            }
-        });
     }
 
-    public void updateSuggestions(List<String> list){
+    public void updateSuggestions(List<String> list) {
         mList = list;
-        if (mFl!=null){// 搜索框文字变化太快,先于onCreate执行(偶现)
-            mFl.setAdapter(new TagAdapter<String>(mList)
-            {
-                @Override
-                public View getView(FlowLayout parent, int position, String s)
-                {
-                    TextView tv = (TextView) LayoutInflater.from(getContext()).inflate(R.layout.item_search_word_hot,
-                            mFl, false);
-                    tv.setText(s);
-                    return tv;
-                }
-            });
+        if (mFl != null) {
+            mFl.removeAllViews();
+            for (int i = 0; i < mList.size(); i++) {
+                final int position = i;
+                final String text = mList.get(i);
+                TextView tv = (TextView) LayoutInflater.from(getContext()).inflate(R.layout.item_search_word_hot, mFl, false);
+                tv.setText(text);
+                tv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (onSelectListener != null) {
+                            onSelectListener.onSelect(position, text);
+                        }
+                    }
+                });
+                mFl.addView(tv);
+            }
         }
     }
 }
